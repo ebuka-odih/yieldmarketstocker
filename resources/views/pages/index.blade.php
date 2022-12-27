5153f487e149d582124be9fa1a664b77fd7cbffe
Gnosis <gnosis@Gnosis-MacBook-Pro.local>
2022-12-27 12:56:27 +0100

    commit changes

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -96,7 +96,146 @@
         <div class="container hero-plus-top">
             <div class="_8-column">
                 <div id="w-node-bd975e25-5829-2d83-c94f-a23de674f156-f84df573" class="content-image-left">
-                    <img src="https://uploads-ssl.webflow.com/630df394ff44d46a174df570/632a4c51371ecd05fb449a8a_section2.png" loading="lazy" alt="" class="content-section-side-image-left" />
+                    <!-- TradingView Widget BEGIN -->
+                    <div class="tradingview-widget-container">
+                        <div class="tradingview-widget-container__widget"></div>
+                        <div class="tradingview-widget-copyright"><a href="https://www.tradingview.com/markets/" rel="noopener" target="_blank"><span class="blue-text">World markets</span></a> by TradingView</div>
+                        <script type="text/javascript" src="https://s3.tradingview.com/external-embedding/embed-widget-market-overview.js" async>
+                            {
+                                "colorTheme": "light",
+                                "dateRange": "12M",
+                                "showChart": true,
+                                "locale": "en",
+                                "largeChartUrl": "",
+                                "isTransparent": false,
+                                "showSymbolLogo": true,
+                                "showFloatingTooltip": false,
+                                "width": "400",
+                                "height": "660",
+                                "plotLineColorGrowing": "rgba(41, 98, 255, 1)",
+                                "plotLineColorFalling": "rgba(41, 98, 255, 1)",
+                                "gridLineColor": "rgba(240, 243, 250, 0)",
+                                "scaleFontColor": "rgba(106, 109, 120, 1)",
+                                "belowLineFillColorGrowing": "rgba(41, 98, 255, 0.12)",
+                                "belowLineFillColorFalling": "rgba(41, 98, 255, 0.12)",
+                                "belowLineFillColorGrowingBottom": "rgba(41, 98, 255, 0)",
+                                "belowLineFillColorFallingBottom": "rgba(41, 98, 255, 0)",
+                                "symbolActiveColor": "rgba(41, 98, 255, 0.12)",
+                                "tabs": [
+                                {
+                                    "title": "Indices",
+                                    "symbols": [
+                                        {
+                                            "s": "NASDAQ:TSLA"
+                                        },
+                                        {
+                                            "s": "NASDAQ:AAPL"
+                                        },
+                                        {
+                                            "s": "NASDAQ:AMZN"
+                                        },
+                                        {
+                                            "s": "NASDAQ:META"
+                                        }
+                                    ],
+                                    "originalTitle": "Indices"
+                                },
+                                {
+                                    "title": "Futures",
+                                    "symbols": [
+                                        {
+                                            "s": "CME_MINI:ES1!",
+                                            "d": "S&P 500"
+                                        },
+                                        {
+                                            "s": "CME:6E1!",
+                                            "d": "Euro"
+                                        },
+                                        {
+                                            "s": "COMEX:GC1!",
+                                            "d": "Gold"
+                                        },
+                                        {
+                                            "s": "NYMEX:CL1!",
+                                            "d": "Crude Oil"
+                                        },
+                                        {
+                                            "s": "NYMEX:NG1!",
+                                            "d": "Natural Gas"
+                                        },
+                                        {
+                                            "s": "CBOT:ZC1!",
+                                            "d": "Corn"
+                                        }
+                                    ],
+                                    "originalTitle": "Futures"
+                                },
+                                {
+                                    "title": "Bonds",
+                                    "symbols": [
+                                        {
+                                            "s": "CME:GE1!",
+                                            "d": "Eurodollar"
+                                        },
+                                        {
+                                            "s": "CBOT:ZB1!",
+                                            "d": "T-Bond"
+                                        },
+                                        {
+                                            "s": "CBOT:UB1!",
+                                            "d": "Ultra T-Bond"
+                                        },
+                                        {
+                                            "s": "EUREX:FGBL1!",
+                                            "d": "Euro Bund"
+                                        },
+                                        {
+                                            "s": "EUREX:FBTP1!",
+                                            "d": "Euro BTP"
+                                        },
+                                        {
+                                            "s": "EUREX:FGBM1!",
+                                            "d": "Euro BOBL"
+                                        }
+                                    ],
+                                    "originalTitle": "Bonds"
+                                },
+                                {
+                                    "title": "Forex",
+                                    "symbols": [
+                                        {
+                                            "s": "FX:EURUSD",
+                                            "d": "EUR/USD"
+                                        },
+                                        {
+                                            "s": "FX:GBPUSD",
+                                            "d": "GBP/USD"
+                                        },
+                                        {
+                                            "s": "FX:USDJPY",
+                                            "d": "USD/JPY"
+                                        },
+                                        {
+                                            "s": "FX:USDCHF",
+                                            "d": "USD/CHF"
+                                        },
+                                        {
+                                            "s": "FX:AUDUSD",
+                                            "d": "AUD/USD"
+                                        },
+                                        {
+                                            "s": "FX:USDCAD",
+                                            "d": "USD/CAD"
+                                        }
+                                    ],
+                                    "originalTitle": "Forex"
+                                }
+                            ]
+                            }
+                        </script>
+                    </div>
+                    <!-- TradingView Widget END -->
+{{--                    <img src="https://uploads-ssl.webflow.com/630df394ff44d46a174df570/632a4c51371ecd05fb449a8a_section2.png" loading="lazy" alt="" class="content-section-side-image-left" />--}}
                 </div>
                 <div id="w-node-bd975e25-5829-2d83-c94f-a23de674f143-f84df573" class="content-block-text-container">
                     <div class="_3em-bottom-margin">
