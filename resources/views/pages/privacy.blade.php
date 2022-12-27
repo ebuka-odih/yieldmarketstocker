43f50fa35719a5b84e917d0bf1c33e2fd349c736
Gnosis <gnosis@Gnosis-MacBook-Pro.local>
2022-12-27 17:49:06 +0100

    commit changes

--- resources/views/pages/privacy.blade.php
+++ resources/views/pages/privacy.blade.php
@@ -56,35 +56,6 @@
 
         </div>
     </div>
-    <div class="section background-tint">
-        <div class="container large-top">
-            <div class="_8-column">
-                <div id="w-node-c855d768-7719-f18a-a3c4-220f6da454bc-2f9a8be8" class="hero-center">
-                    <div class="hero-stars-lockup-1-center _2-5em-bottom-margin">
-                        <div class="stars-1">
-                            <div class="full-green-star"></div>
-                            <div class="full-green-star"></div>
-                            <div class="full-green-star"></div>
-                            <div class="full-green-star"></div>
-                            <div class="full-green-star"></div>
-                        </div>
-                        <div class="dark-dimmed-text">4.7 • <a href="https://www.trustpilot.com/review/tradezella.com" target="_blank">TrustPilot Reviews</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="container large-bottom">
-            <div class="logo-grid">
-                <div id="w-node-_1fa9ffda-b367-07c5-58f2-16e0394987bc-2f9a8be8" class="logo-grid-image _1"></div>
-                <div id="w-node-_1fa9ffda-b367-07c5-58f2-16e0394987bd-2f9a8be8" class="logo-grid-image _2"></div>
-                <div id="w-node-_1fa9ffda-b367-07c5-58f2-16e0394987be-2f9a8be8" class="logo-grid-image _3"></div>
-                <div id="w-node-_1fa9ffda-b367-07c5-58f2-16e0394987bf-2f9a8be8" class="logo-grid-image _4"></div>
-                <div id="w-node-_1fa9ffda-b367-07c5-58f2-16e0394987c0-2f9a8be8" class="logo-grid-image _5"></div>
-                <div id="w-node-_1fa9ffda-b367-07c5-58f2-16e0394987c1-2f9a8be8" class="logo-grid-image _6"></div>
-            </div>
-        </div>
-    </div>
 
 
 @endsection
